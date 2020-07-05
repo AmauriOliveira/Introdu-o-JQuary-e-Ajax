b6f3168137b9cba24b7c76704f258cb6394f4a20
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-07-05 15:34:50 -0300

    Finalização com barra de progresso e tabelas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 function consultaCep() {
 
+    $(".bar-progress").show();
+
     const cep = document.getElementById('CEP').value;
     const url = 'https://viacep.com.br/ws/' + cep + '/json/'
 
@@ -10,14 +12,22 @@ function consultaCep() {
             console.log(data);
 
             $("#bairro").html(data.bairro);
-            $("#cep").html(data.cep);
-            $("#complemento").html(data.complemento);
-            $("#gia").html(data.gia);
-            $("#ibge").html(data.ibge);
+            $("#cep").html("CEP - " + data.cep);
+            // $("#complemento").html(data.complemento);
+            // $("#gia").html(data.gia);
+            // $("#ibge").html(data.ibge);
             $("#localidade").html(data.localidade);
             $("#logradouro").html(data.logradouro);
             $("#uf").html(data.uf);
             $("#unidade").html(data.unidade);
+            
+            $(".bar-progress").hide();
+            $(".cep").show();
         }
     })
-}
+};
+
+$(function () {
+    $(".cep").hide();
+    $(".bar-progress").hide();
+});
